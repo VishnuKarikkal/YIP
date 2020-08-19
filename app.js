61f072aeb1954dbf7b9825bb704b4b89586609c9
vivek <vivek.appu619@gmail.com>
2020-08-19 18:36:33 +0530

    adding game routes

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const path = require("path");
 const cors = require('cors');
 const checkAuth = require('./src/middleware/check-auth')
 const bodyParser = require('body-parser');
-const loginRouter=require('./src/routes/loginRouter')
+const loginRouter=require('./src/routes/loginRouter');
+const gameRouter = require('./src/routes/gameRouter')
 const app = new express();
 app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({extended: true}))
-app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.set("view engine", "html");
 app.set("views", "./src/views");
 app.use(cors());
 app.use(express.static("./public"));
+
 const port=process.env.PORT||5000;
-app.use('login',loginRouter)
+app.use('/login',loginRouter);
+app.use('/game',gameRouter);
 app.get('/',function(req,res){
     res.send('hi');
 })
+
 app.listen(port, () => {
     console.log(`Server Listening On Port:${port}`);
 });
---
+++ src/routes/gameRouter.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const gameRouter = express.Router();
+gameRouter.post('/vote',function (req,res) {
+
+})
+module.exports=gameRouter;
