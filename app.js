9dc753613ff6ff1d8076777ff7a7c1709d330304
VishnuKarikkal <sajanvishnu0@gmail.com>
2020-08-19 12:31:11 +0530

    file structure  changes + app.js changes + partial TEAM page

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const cors = require('cors');
+const checkAuth=require('./src/middleware/check-auth')
+const bodyParser = require('body-parser')
+const app = new express();
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.json());
+app.set("view engine", "html");
+//app.set("views", "./src/views");
+
+app.set('views',__dirname+"/src/views");
+app.use(cors());
+
+//app.use('',api);
+app.get('/',(req,res)=>
+                        {
+                           res.sendFile(__dirname+'/src/views/team.html');
+                        });
+
+app.listen(3232,()=>
+                    {
+                        console.log("Server Listening On Port:3232");
+                    });
+
+//const port=process.env.PORT||5000;
