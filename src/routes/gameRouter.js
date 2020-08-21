f87d08155a063c1399932463890736d1e366110f
VishnuKarikkal <sajanvishnu0@gmail.com>
2020-08-21 23:22:34 +0530

    added gameRouter dummies

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -10,8 +10,24 @@ gameRouter.get("/activeGame", function (req, res) {
   
   res.send({message:"none"})    //if none is active
 });
-
+gameRouter.post("/activate",(req,res)=>
+{
+  //to activate the months selected by the admin while clicking 'activate' btn
+  
+});
 gameRouter.post("/vote", function (req, res) {
     //to save the vote of a team into the database : to 'gameHistorys' collection
 });
+gameRouter.post("/publish",(req,res)=>
+{
+  //to publish the votes of the teams to calculate the results {determines whether contribution or gain}
+  //changes 'gameHistorys' collection(amount,isContribute,totalBalance) + 'teamSummary' collection(balance)
+
+});
+gameRouter.post("/addRemarks",(req,res)=>
+{
+  //to add bonus/penalty to the balances of each team whose votes are published
+  //changes 'gameHistorys' collection(ramarks) + 'teamSummary' collection(balance)
+
+})
 module.exports = gameRouter;
