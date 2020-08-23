d273bf8ea94f2a649d401f760f1f1d43f92de9d4
vivek <vivek.appu619@gmail.com>
2020-08-23 19:41:51 +0530

    signin

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_KEY||'secret');
     req.userData = decoded;
     next();
   } catch (error) {
--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -9,7 +9,8 @@ loginRouter.get("/", function (req, res) {
 });
 loginRouter.post("/", function (req, res) {
   console.log(req.body);
-  let username=req.body.username;
+  console.log('here');
+  let username=req.body.username.toUpperCase();
   let password=req.body.password;
   console.log(username)
   console.log(password)
@@ -26,11 +27,11 @@ loginRouter.post("/", function (req, res) {
         });
       } else {
        // checking wether password matches
-          console.log('here',password,user[0].password);
+          console.log('here',user[0]);
          if(password===user[0].password){
             const token = jwt.sign(
               {
-                username: user[0].name,
+                username: user[0].username,
               },
               jwtKey,
               {
@@ -42,9 +43,9 @@ loginRouter.post("/", function (req, res) {
               message: "Auth successful",
               redirect: true,
               token: token,
+                teamName:user[0].username,
             });
           } else {
-            console.log("failed")
             console.log(user[0].password)
             return res.status(401).json({
               message: "Auth failed wrong password",
@@ -55,52 +56,5 @@ loginRouter.post("/", function (req, res) {
 
         });
 
-/*---------------------code to handle password change request------------------------*/
-loginRouter.post("/changePassword", function (req, res) {
-  user
-    .find({ id: req.body._id })
-    .exec()
-    .then((user) => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth failed user not present in database",
-        });
-      } else {
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if (err) {
-            return res.status(401).json({
-              message: "Auth failed",
-            });
-          } else if (result) {
-            const token = jwt.sign(
-              {
-                userId: user[0]._id,
-                username: user[0].name,
-              },
-              process.env.JWT_KEY,
-              {
-                expiresIn: "1hr",
-              }
-            );
-            console.log("authsuccessful");
-            return res.status(200).json({
-              message: "Auth successful",
-              redirect: true,
-              token: token,
-            });
-          } else {
-            return res.status(401).json({
-              message: "Auth failed wrong password",
-            });
-          }
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+
 module.exports = loginRouter;
